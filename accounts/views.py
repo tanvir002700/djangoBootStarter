e4640bee31fec77f0532067c0f62f876558a82ec
tanvir002700 <tanvir002700@gmail.com>
2017-07-23 14:24:41 +0600

    add login form

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic.edit import FormView
+from .forms import LoginForm
 
-# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+
+class LoginView(FormView):
+    template_name = 'login.html'
+    form_class = LoginForm
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
