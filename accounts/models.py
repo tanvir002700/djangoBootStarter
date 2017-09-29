d449a35ad77acc5e7ce43da8925bec333cecefa8
tanvir002700 <tanvir002700@gmail.com>
2017-09-30 00:52:17 +0600

    add Timestamps to accounts model

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from core.models import TimeStampedModel
 
 
-class User(AbstractUser):
+class User(AbstractUser, TimeStampedModel):
     date_of_birth = models.DateField(null=True)
     avatar = models.ImageField(upload_to='avatar', null=True,  blank=True)
 
