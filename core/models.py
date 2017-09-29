a49dcf815e1b5a798a68765027bc33954c629b56
tanvir002700 <tanvir002700@gmail.com>
2017-09-30 00:54:31 +0600

    code refactor

--- core/models.py
+++ core/models.py
@@ -4,5 +4,7 @@ from django.db import models
 class TimeStampedModel(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+
     class Meta:
         abstract = True
+
